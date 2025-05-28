ffe61a35073c8682f2c6d24ddc1efe91ee08816b
ambyyousaf23 <amby98yousaf@gmail.com>
2025-05-28 18:10:22 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  base: '/Hamza_cv/',
+  site: 'https://ambyyousaf23.github.io/Hamza_cv/',
+  base: '/docs'
+  
 });
