34651208ec48cbc6dc6c716c906f8b21b3b64450
ambyyousaf23 <amby98yousaf@gmail.com>
2025-05-28 15:37:48 +0500

    sxnks
    sks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  outDir: 'docs',  // change output folder from 'dist' to 'docs'
-  base: 'https://github.com/ambyyousaf23/Hamza_cv',      // relative URLs for assets
+  outDir: 'docs',
+  base: '/Hamza_cv/', // <- Only the path part!
 });
