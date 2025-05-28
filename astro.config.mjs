1a02c9e5826675470b30ecbdd194826457c73485
ambyyousaf23 <amby98yousaf@gmail.com>
2025-05-28 16:49:18 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,10 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   outDir: 'docs',
-  base: '/Hamza_cv/', // <- Only the path part!
+  base: '/Hamza_cv/',
+  vite: {
+    build: {
+      cssCodeSplit: true, // This forces CSS files to be separate
+    },
+  },
 });
