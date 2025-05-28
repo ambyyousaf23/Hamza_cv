22639fd320476b511904429b2948f040a3fd1483
ambyyousaf23 <amby98yousaf@gmail.com>
2025-05-28 21:41:02 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: '/docs',
+  site: 'https://ambyyousaf23.github.io/Hamza_cv', // Required for relative asset paths
+  base: '/Hamza_cv/', // <--- This is important!
 });
